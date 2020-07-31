ccdcc4e7a027b734f2f2573c4427fce3447f6b0e
laurenmbotha <laurenmbotha@gmail.com>
2020-07-31 22:14:59 +0000

    Added Register Driver

---
+++ capstone/src/main/java/com/google/sps/data/Driver.java
@@ -0,0 +1,21 @@
+package com.google.sps.data;
+
+/** An item on a todo list. */
+public final class Driver {
+
+  private final String first;
+  private final String last;
+  private final String day;
+  private final String times;
+  private final long seats;
+  private final long timestamp;
+
+  public Driver(String first, String last, String day, String times, long seats, long timestamp) {
+    this.first = first;
+    this.last = last;
+    this.day = day;
+    this.times = times;
+    this.seats = seats;
+    this.timestamp = timestamp;
+  }
+}
